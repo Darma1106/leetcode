047e756617da86ec7593c13fd95d958ccc6ebef8
darma <1946498212@qq.com>
2021-08-29 23:39:47 +0800

    feat: 颜色转换

--- 简单/颜色转换.ts
+++ 简单/颜色转换.ts
@@ -1,23 +1,25 @@
 function colorToBytes(color: string, alpha: number = 255): number[] {
-  let bytesColor: number[] = [];
+  let bytesColor: number[] = []
   for (let index = 0; index < 3; index++) {
-    bytesColor.push(parseInt(color.substr(1 + 2 * index, 2), 16));
+    bytesColor.push(parseInt(color.substr(1 + 2 * index, 2), 16))
   }
-  bytesColor.push(alpha);
-  return bytesColor;
+  bytesColor.push(alpha)
+  return bytesColor
 }
 
 function rgbaToBytes(color: string): number[] {
-  let bytesColor: number[] = [];
-  let colorItem = color.substr(5, color.length - 6).split(",");
+  let bytesColor: number[] = []
+  let colorItem = color.substr(5, color.length - 6).split(',')
   for (let index = 0; index < 4; index++) {
-    let element = Number(colorItem[index]);
+    let element = Number(colorItem[index])
     if (index == 3) {
-      element *= 255;
+      element *= 255
     }
-    bytesColor.push(element);
+    bytesColor.push(element)
   }
-  return bytesColor;
+  return bytesColor
 }
 
-console.log(rgbaToBytes("rgba(230, 33, 31, 0.63)"));
+const a = [1, 2, 3]
+a.forEach
+console.log(rgbaToBytes('rgba(230, 33, 31, 0.63)'))
