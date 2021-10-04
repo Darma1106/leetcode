3337fa40ea1de37f258db839c81c3e9a1aeffd39
darma <darmabigma@gmail.com>
2021-10-04 15:35:30 +0800

    fix(209): 优化滑动窗口

--- 中等/leetcode-209.ts
+++ 中等/leetcode-209.ts
@@ -3,17 +3,20 @@
 // 找出该数组中满足其和 ≥ target 的长度最小的 连续子数组 [numsl, numsl+1, ..., numsr-1, numsr] ，并返回其长度。如果不存在符合条件的子数组，返回 0 。
 
 function minSubArrayLen(target: number, nums: number[]): number {
-  let i = 0,
-    sum = 0,
-    len = 0
-  for (let j = 0; j < nums.length; j++) {
-    sum += nums[j]
+  let left = 0,
+    len = 0,
+    sum = 0
+  for (let right = 0; right < nums.length; right++) {
+    // 窗口右侧右滑
+    sum += nums[right]
     while (sum >= target) {
-      len = len == 0 ? j - i + 1 : Math.min(len, j - i + 1)
-      sum -= nums[i++]
+      // 更新len,窗口左侧右滑
+      len = len == 0 ? right - left + 1 : Math.min(right - left + 1, len)
+      sum -= nums[left]
+      left++
     }
   }
   return len
 }
 
-console.log(minSubArrayLen(15, [1, 2, 3, 4, 5]))
+console.log(minSubArrayLen(5, [1, 2, 3, 5, 1, 1, 1, 1]))
