537cf2e1d9ead0e1ee16a1573a671dfc4c687a68
darma <damabigma@gmail.com>
2021-10-05 20:34:14 +0800

    fix(leetcode3): 调整方法结构

--- 中等/leetcode-3.ts
+++ 中等/leetcode-3.ts
@@ -1,22 +1,21 @@
 // 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
 function lengthOfLongestSubstring(s: string): number {
   let len = s.length,
-    result = 0,
+    res = 0,
     set = new Set(),
     left = 0,
     right = 0
 
   while (left < len) {
-    // 右侧右滑
     while (right < len && !set.has(s[right])) {
       set.add(s[right])
       right++
     }
-    // 左侧右滑
-    result = Math.max(result, right - left)
+    res = Math.max(res, right - left)
     set.delete(s[left])
     left++
   }
-  return result
+
+  return res
 }
-console.log(lengthOfLongestSubstring('aabca'))
+console.log(lengthOfLongestSubstring(' '))
